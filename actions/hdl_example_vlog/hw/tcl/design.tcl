351950ecf3ad1ed09e72f948e3165017585d4056
xpower2d <xdxing@cn.ibm.com>
2018-07-20 14:06:06 +0800

    Update design.tcl
    changed directory, left only FIFO IP.

--- actions/hdl_example_vlog/hw/tcl/design.tcl
+++ actions/hdl_example_vlog/hw/tcl/design.tcl
@@ -1,20 +1,11 @@
 set action_hw $::env(ACTION_ROOT)/hw
-set verilog_dir  $::env(ACTION_ROOT)/hw/verilog
+set verilog_dir  $::env(ACTION_ROOT)
 
 add_files -scan_for_includes -norecurse $action_hw
-add_files -scan_for_includes -norecurse $verilog_dir/snap_adapter
-add_files -scan_for_includes -norecurse $verilog_dir/core
 
 #User IPs
 foreach usr_ip [list \
-                $verilog_dir/core/ip/bram_1744x16                   \
-                $verilog_dir/core/ip/bram_dual_port_512x32          \
-                $verilog_dir/core/ip/fifo_48x16_async               \
-                $verilog_dir/core/ip/fifo_512x64_sync_bram          \
-                $verilog_dir/core/ip/fifo_80x16_async               \
-                $verilog_dir/core/ip/unit_fifo_48x16_async          \
-                $verilog_dir/snap_adapter/ip/fifo_sync_32_512i512o  \
-                $verilog_dir/snap_adapter/ip/ram_512i_512o_dual_64  \
+                $verilog_dir/ip/fifo_sync_32_512i512o  \
                ] {
   foreach usr_ip_xci [exec find $usr_ip -name *.xci] {
     puts "                        importing user IP $usr_ip_xci (in string_match core)"
@@ -23,4 +14,3 @@ foreach usr_ip [list \
     export_ip_user_files -of_objects  [get_files "$usr_ip_xci"] -force >> $log_file
   }
 }
-
